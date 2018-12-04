da616cc14bb58d85c6b14c6c271b5222a084efda
Andrzej Gdula <andrzej.gdula@capsilon.com>
2018-12-04 09:30:28 +0100

    fix move all jna 4.5.1

--- auto-itj-local/src/main/java/cn/com/jautoitx/impl/AutoItXImpl.java
+++ auto-itj-local/src/main/java/cn/com/jautoitx/impl/AutoItXImpl.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -57,7 +58,7 @@ public class AutoItXImpl implements AutoItX {
 
 			logger.fine("AutoItX initialized.");
 		} catch (Throwable e) {
-			logger.warning("Unable to initialize "
+			logger.log(Level.SEVERE, "Unable to initialize "
 					+ AutoItXImpl.class.getSimpleName());
 		}
 	}
